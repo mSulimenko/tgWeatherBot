4a8d717bd0644d17569b85025e0834fc1d140fcc
Max <sulimenko.maxim@gmail.com>
2025-10-16 20:48:41 +0700

    Add config example

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,6 @@ import "github.com/ilyakaznacheev/cleanenv"
 type Config struct {
 	TgAPIToken    string `yaml:"tg_api_token"`
 	WeatherAPIKey string `yaml:"weather_api_key"`
-	LogLevel      string `yaml:"log_level"`
 }
 
 func MustReadConfig(configPath string) *Config {
